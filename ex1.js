664887faa2996d84acc15116fe92d883c14c9828
Rogério Maciel de Souza <89948453+rogermsouza@users.noreply.github.com>
2023-08-17 02:31:47 -0300

    Update ex1.js

--- ex1.js
+++ ex1.js
@@ -2,16 +2,16 @@ function exerc1() {
     let valor = prompt("Digite o valor: ");
     if(confirm("Deseja conferir o tipo de dado?")){
         if(isNaN(valor)){
-        	alert("O valor é uma string");
-        }
-	else {
-		alert("O valor é um numero");
+            alert("O valor é uma string");
+        }else{
+		 alert("O valor é um numero");
 	}
-   }
-    else {
+} else {
 	    alert("Obrigado por visitar essa página");
    }
 }
+
+
 
 function par(){
 	let numero = prompt("Digite um valor inteiro");
