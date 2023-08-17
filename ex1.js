657149047e43bb2adb51145d5b65aefbda13e065
Rogério Maciel de Souza <89948453+rogermsouza@users.noreply.github.com>
2023-08-17 02:17:28 -0300

    Corrigindo o primeiro exercicio

--- ex1.js
+++ ex1.js
@@ -1,21 +1,11 @@
 function exerc1() {
     let valor = prompt("Digite o valor: ");
     if(confirm("Deseja conferir o tipo de dado?")){
-        if(typeof valor === "number"){
-            alert("O valor é um numero");
-        }
-        if(typeof valor === "string"){
+        if(isNaN(valor)){
             alert("O valor é uma string");
-        }
-        if(typeof valor === "object"){
-            alert("O valor digitado é um objeto")
-        }
-        if(typeof valor === "undefined"){
-            alert("O valor digitado não está definido");
-        }
-    }else{
-        document.write("Obrigado por visitar essa página;");
-    }
+        }else{
+		 alert("O valor é um numero");
+	}
 }
 
 function par(){
